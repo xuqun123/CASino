b904c1513d77393580db5df0b736ca2965f09c23
Qun Xu <xuqunlele@gmail.com>
2017-07-20 18:21:26 +0930

    [FEATURE] Single sign-on (SSO) - Centralised Authentication (exclude Shopify): 1. add get ticket api request

--- app/api/casino/api/resource/auth_token_tickets.rb
+++ app/api/casino/api/resource/auth_token_tickets.rb
@@ -1,12 +1,19 @@
 require 'grape'
 
 class CASino::API::Resource::AuthTokenTickets < Grape::API
+
   resource :auth_token_tickets do
     desc 'Create an auth token ticket'
     post do
       @ticket = CASino::AuthTokenTicket.create
       Rails.logger.debug "Created auth token ticket '#{@ticket.ticket}'"
       present @ticket, with: CASino::API::Entity::AuthTokenTicket
+    end
+
+    get do
+      @ticket_granting_ticket = CASino::TicketGrantingTicket.where(ticket: cookies[:tgt]).first
+      Rails.logger.debug "Retrieving ticket granting ticket #{cookies[:tgt]}: '#{@ticket_granting_ticket.try(:ticket)}'"
+      present @ticket_granting_ticket, with: CASino::API::Entity::AuthTokenTicket
     end
   end
 end
